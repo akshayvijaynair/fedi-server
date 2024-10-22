e54bde92526863cf0e1bf01def821bc2ffe35b9e
kadeemmiller33 <millerkadeem42@gmail.com>
2024-10-22 13:53:26 -0400

    removed table

--- Project/Capstone-MVP/src/main/java/com/PaceUniversity/Capstone/MVP/entity/UserTable.java
+++ Project/Capstone-MVP/src/main/java/com/PaceUniversity/Capstone/MVP/entity/UserTable.java
@@ -1,10 +1,7 @@
 package com.PaceUniversity.Capstone.MVP.entity;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -12,14 +9,12 @@ import lombok.Setter;
 import java.time.LocalDate;
 
 @Entity
-public class UserTable(name = _"user"){
+@Table
+public class UserTable(){
 
 
     @Id
-    @Getter
-    @Setter
-    @NoArgsConstructor
-    @GeneratedValue;
+
     Long id;
 
     String username;
